9298138d768c4196aac522cb6d22b80cbd32f1f0
jie-L-star <1456495162@qq.com>
2024-01-21 14:55:41 +0800

    修改upload.py, token为函数输入

--- learning_code/upload.py
+++ learning_code/upload.py
@@ -1,41 +1,42 @@
 from github import Github
 import os
 
-token = "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"
-g = Github(token)
 
-repo_name = "kaggle_output"
-repo = g.get_user().get_repo(repo_name)
+def my_upload(token):
+    g = Github(token)
 
-# 定义文件夹和文件类型的映射关系
-folder_mapping = {
-    ".txt": "log_files",
-    ".pth": "model_files",
-    ".jpg": "los_image_files"
-}
+    repo_name = "kaggle_output"
+    repo = g.get_user().get_repo(repo_name)
 
-local_folder = "/kaggle/working/"
-for file_name in os.listdir(local_folder):
-    if os.path.isfile(os.path.join(local_folder, file_name)):
-        file_extension = os.path.splitext(file_name)[1].lower()
+    # 定义文件夹和文件类型的映射关系
+    folder_mapping = {
+        ".txt": "log_files",
+        ".pth": "model_files",
+        ".jpg": "los_image_files"
+    }
 
-        # 确保文件类型在映射关系中
-        if file_extension in folder_mapping:
-            folder_name = folder_mapping[file_extension]
+    local_folder = "/kaggle/working/"
+    for file_name in os.listdir(local_folder):
+        if os.path.isfile(os.path.join(local_folder, file_name)):
+            file_extension = os.path.splitext(file_name)[1].lower()
 
-            # 获取目标文件夹中现有文件的数量
-            existing_files = repo.get_contents(folder_name)
-            num_files = len(existing_files)
+            # 确保文件类型在映射关系中
+            if file_extension in folder_mapping:
+                folder_name = folder_mapping[file_extension]
 
-            # 构建新文件的名称
-            new_file_name = f"{file_name.split('.')[0]}{num_files}{file_extension}"
-            remote_file_path = f"{folder_name}/{new_file_name}"
+                # 获取目标文件夹中现有文件的数量
+                existing_files = repo.get_contents(folder_name)
+                num_files = len(existing_files)
 
-            # 读取本地文件的内容
-            with open(os.path.join(local_folder, file_name), 'rb') as file:
-                file_content = file.read()
+                # 构建新文件的名称
+                new_file_name = f"{file_name.split('.')[0]}{num_files}{file_extension}"
+                remote_file_path = f"{folder_name}/{new_file_name}"
 
-            # 在仓库中创建或更新文件
-            repo.create_file(remote_file_path, f"Upload {remote_file_path}", file_content, branch="main")
+                # 读取本地文件的内容
+                with open(os.path.join(local_folder, file_name), 'rb') as file:
+                    file_content = file.read()
 
-print("Files uploaded successfully.")
+                # 在仓库中创建或更新文件
+                repo.create_file(remote_file_path, f"Upload {remote_file_path}", file_content, branch="main")
+
+    print("Files uploaded successfully.")
